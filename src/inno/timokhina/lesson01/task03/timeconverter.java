9dab4e4fecd59c4d807a08ad04b2013b01c36a03
Julia Timokhina <julia.timokhina604@gmail.com>
2020-06-05 23:44:00 +0300

    task3

---
+++ src/inno/timokhina/lesson01/task03/timeconverter.java
@@ -0,0 +1,9 @@
+package inno.timokhina.lesson01.task03;
+
+public class timeconverter {
+    public static void main(String[] args) {
+        int sec = 3600; // Секунды
+        int hour = sec / 3600;// Минуты
+        System.out.println(sec + " секунд - это  " + hour + " минута.");
+    }
+}
